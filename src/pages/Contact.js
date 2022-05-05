da2504ebf067bc5ea839e2d8489a49b3d737f238
Lisitius <90479443+Lisitius@users.noreply.github.com>
2022-05-05 16:51:58 +0200

    Showcase site with react and sass

---
+++ src/pages/Contact.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { ContactForm } from "../components/ContactForm";
+import Logo from "../components/Logo";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import SocialNetwork from "../components/SocialNetwork";
+import Buttons from "../components/Buttons";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 200,
+    },
+  };
+
+  return (
+    <main>
+      <Mouse />
+      <motion.div
+        initial="out"
+        animate="in"
+        exit="out"
+        variants={pageTransition}
+        transition={{ duration: 0.5 }}
+        className="contact"
+      >
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>Adresse</h4>
+              <p>12 rue très fausse</p>
+              <p>85242 Quelque part</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>Téléphone</h4>
+              <CopyToClipboard text="0606060606" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Téléphone copié !")}
+                >
+                  0606060606
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>Email</h4>
+              <CopyToClipboard text="Fake@gmail.com" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Email copié !")}
+                >
+                  Fake@gmail.com
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetwork />
+          <div className="credits">
+            <p>Merci à FromScratch</p>
+          </div>
+        </div>
+        <Buttons left={"/projet-4"} />
+      </motion.div>
+    </main>
+  );
+};
+
+export default Contact;
